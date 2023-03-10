812aff2a2902e1183e3c38b18eeb942017058bd2
hjflemin <89958378+hjflemin@users.noreply.github.com>
2023-03-10 17:38:07 -0600

    Reorganization for single eye testing and future ui integration

--- Assets/PatientTest/Scripts/MainCameraHandler.cs
+++ Assets/PatientTest/Scripts/MainCameraHandler.cs
@@ -4,11 +4,102 @@ using UnityEngine;
 
 public class MainCameraHandler : MonoBehaviour
 {
+    [SerializeField]
+    private bool right;
+
+    [SerializeField]
+    private bool left;
+
+    private bool set = false;
+
+    [SerializeField]
+    private GameObject[] rightObjects;
+    
+    [SerializeField]
+    private GameObject[] leftObjects;
+
+    public void setRight()
+    {
+        right = true;
+        left = false;
+        Activation();
+    }
+
+    public void Activation()
+    {
+        if(left)
+        {
+            foreach(GameObject i in leftObjects)
+                {
+                    i.SetActive(true);
+                }
+            set = true;
+        }
+        else if(right)
+        {
+            foreach(GameObject i in rightObjects)
+            {
+                i.SetActive(true);
+            }
+            set = true;
+        }
+    }
+
+    public void Deactivation(bool l)
+    {
+        if(l)
+        {
+            foreach(GameObject i in leftObjects)
+            {
+                i.SetActive(false);
+            }
+            set = false;
+        }
+        else
+        {
+            foreach(GameObject i in rightObjects)
+            {
+                i.SetActive(false);
+            }
+            set = false;
+        }
+    }
+    public void setLeft()
+    {
+        right = false;
+        left = true;
+        Activation();
+    }
+
+    public void updateRight()
+    {
+        if(!set)
+            setRight();
+        else
+        {
+            Deactivation(true);
+            setRight();
+        }
+    }
+
+    public void updateLeft()
+    {
+        if(!set)
+            setLeft();
+        else
+        {
+            Deactivation(false);
+            setLeft();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        transform.GetChild(0).gameObject.SetActive(true);
-        transform.GetChild(1).gameObject.SetActive(true);
+        if(left)
+            setLeft();
+        else
+            setRight();
     }
 
     // Update is called once per frame
