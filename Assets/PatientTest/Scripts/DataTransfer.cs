d69a22961618bd94a8c0e1840d0a03a3f8a2a4ad
Keegeey <goode.grant@gmail.com>
2023-03-29 18:21:42 -0500

    Added variables to transfer to PDF in DataTransfer file

--- Assets/PatientTest/Scripts/DataTransfer.cs
+++ Assets/PatientTest/Scripts/DataTransfer.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class DataTransfer : MonoBehaviour
 {
+    //Patient info (may not be necessary, could just put placeholders)
+    public string patientName;
+    public int patientID;
+    public string patientDOB;
+
+    //Test info
     public static List<Vector4> data;
+    public string eyeTested; //Left or right eye
+    public float testStartTime;
+    public float testEndTime;
+    public float testDuration;
+    public string testAlgorithm;
+    public float stimulusSize;
+    public string stimulusColor;
+    public int numPoints;
+    public float errorRate;
 }
